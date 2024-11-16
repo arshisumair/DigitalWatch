c0b5a50aa041354f2fb0166bede395fc5aa6340c
arshisumair <arshisumair38@gmail.com>
2024-11-16 10:06:54 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+function updateClock(){
+  const today = new Date ();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+
+
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('clock').innerHTML = h + ":" + m + ":" + s ;
+  setTimeout(StorageEventTime,1000);
+}
+
+function checkTime(i){
+    if(i < 10){i = "0 " + i}
+    return i;
+}
+
